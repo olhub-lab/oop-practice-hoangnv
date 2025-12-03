26ca08a15f9b5aba798c77fd02d5bfed80af9e6f
HoangHuyDiet <hoangnguyen123425@gmail.com>
2025-12-03 17:22:14 +0700

    [FEAT] Optimizing for bubblesort and quicksort

--- homework2/Sort.java
+++ homework2/Sort.java
@@ -18,13 +18,19 @@ public class Sort {
     }
 
     public static void bubbleSort(int[] a) {
-        for (int i = 0; i < a.length-1; i++) {
+      boolean flag;
+      for (int i = 0; i < a.length-1; i++) {
+            flag = false;
             for (int j = 0; j < a.length - i - 1; j++) {
                 if (a[j] > a[j+1]) {
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
+                    flag = true;
                 }
+            }
+            if (!flag) {
+              break;
             }
         }
     }
