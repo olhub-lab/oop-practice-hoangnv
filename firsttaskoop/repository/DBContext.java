5f8a7c5a787917980c16b35b31dc23bd1318eb39
HoangHuyDiet <hoangnguyen123425@gmail.com>
2026-03-01 13:29:08 +0700

    [FEATURE/CONTROLLER] Initial update for repository

--- firsttaskoop/repository/DBContext.java
+++ firsttaskoop/repository/DBContext.java
@@ -1,35 +1,21 @@
 package firsttaskoop.repository;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBContext {
 
-  private static final HikariDataSource dataSource;
-
-  static {
-    HikariConfig config = new HikariConfig();
-
-    config.setJdbcUrl("jdbc:mysql://localhost:3306/vehicle_management");
-    config.setUsername("root");
-    config.setPassword("145hoang");
-    config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-
-    config.setMaximumPoolSize(10);
-    config.setMinimumIdle(5);
-    config.setIdleTimeout(300000);
-    config.setConnectionTimeout(20000);
-
-    config.addDataSourceProperty("cachePrepStmts", "true");
-    config.addDataSourceProperty("prepStmtCacheSize", "250");
-    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
-    dataSource = new HikariDataSource(config);
-  }
+  private static final String URL = "jdbc:mysql://localhost:3306/vehicle_management";
+  private static final String USER = "root";
+  private static final String PASS = "145hoang";
 
   public static Connection getConnection() throws SQLException {
-    return dataSource.getConnection();
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      return DriverManager.getConnection(URL, USER, PASS);
+    } catch (ClassNotFoundException e) {
+      throw new SQLException("Không tìm thấy Driver JDBC!");
+    }
   }
 }
